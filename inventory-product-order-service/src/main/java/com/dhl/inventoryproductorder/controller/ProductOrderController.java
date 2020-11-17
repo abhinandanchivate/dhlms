4010b9075ee2209876bc085ab87e6a33d8096bae
abhin <abhinandanchivate2000@gmail.com>
2020-11-17 12:36:28 +0530

    @post mapping fixed

--- inventory-product-order-service/src/main/java/com/dhl/inventoryproductorder/controller/ProductOrderController.java
+++ inventory-product-order-service/src/main/java/com/dhl/inventoryproductorder/controller/ProductOrderController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class ProductOrderController {
 
 	@Autowired
 	ProductOrderService productOrderService;
-	
+	@PostMapping
 	public ResponseEntity<Map<String, String>> createProductOrderRequest(
 			@Valid @RequestBody ProductOrderRequest productOrderRequest) {
 		
