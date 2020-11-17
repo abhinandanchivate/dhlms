c105d5949433658698db2d064cae144d63d5369d
abhin <abhinandanchivate2000@gmail.com>
2020-11-17 11:02:59 +0530

    exception part

---
+++ inventory-product-order-service/src/main/java/com/dhl/inventoryproductorder/exception/CustomExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.dhl.inventoryproductorder.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<>
+
+}
