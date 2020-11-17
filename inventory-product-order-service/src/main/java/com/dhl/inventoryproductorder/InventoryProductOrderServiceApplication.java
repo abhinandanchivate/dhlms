f5f5a5050a41552850c892c816b69a9475827b19
abhin <abhinandanchivate2000@gmail.com>
2020-11-17 10:56:37 +0530

    product oerder service

---
+++ inventory-product-order-service/src/main/java/com/dhl/inventoryproductorder/InventoryProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dhl.inventoryproductorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryProductOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryProductOrderServiceApplication.class, args);
+	}
+
+}
